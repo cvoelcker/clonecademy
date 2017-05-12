911b29c2e413940beb3ef705a9f83b389a1e562d
binarybox <lwiedmann@gmx.de>
2017-05-12 20:30:35 +0200

    permissions for the scripts changed

--- bin/setup_scripts.sh
+++ bin/setup_scripts.sh
@@ -31,22 +31,27 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm 2>/dev/null /usr/bin/build_*
+sudo chmod +x $DIR/build_*
 sudo ln -s $DIR"/build_angular.sh" "/usr/bin/build_angular"
 sudo ln -s $DIR"/build_django.sh" "/usr/bin/build_django"
 
 rm 2>/dev/null /usr/bin/start_*
+sudo chmod +x $DIR/start_*
 sudo ln -s $DIR"/start_angular.sh" "/usr/bin/start_angular"
 sudo ln -s $DIR"/start_django.sh" "/usr/bin/start_django"
 
 rm 2>/dev/null /usr/bin/stop_*
+sudo chmod +x $DIR/stop_*
 sudo ln -s $DIR"/stop_angular.sh" "/usr/bin/stop_angular"
 sudo ln -s $DIR"/stop_django.sh" "/usr/bin/stop_django"
 
-rm 2>/dev/null /usr/bin/run*
+rm 2>/dev/null /usr/bin/run_*
+sudo chmod +x $DIR/run_*
 sudo ln -s $DIR"/run_angular.sh" "/usr/bin/run_angular"
 sudo ln -s $DIR"/run_django.sh" "/usr/bin/run_django"
 
 rm 2>/dev/null /usr/bin/clonecademy_*
+sudo chmod +x $DIR/clonecademy_*
 sudo ln -s $DIR"/clonecademy_build.sh" "/usr/bin/clonecademy_build"
 sudo ln -s $DIR"/clonecademy_dev.sh" "/usr/bin/clonecademy_start"
 sudo ln -s $DIR"/clonecademy_stop.sh"  "/usr/bin/clonecademy_stop"
