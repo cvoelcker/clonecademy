eba49ad2870897f795b177dad8ac6085881c5a2f
binarybox <lwiedmann@gmx.de>
2017-05-15 14:40:16 +0200

    fixed a bug with npm install in docker-compose

--- bin/build_angular.sh
+++ bin/build_angular.sh
@@ -5,3 +5,4 @@ DIR=$(find / -type d -name 'clonecadamy' 2>/dev/null -print -quit)
 cd $DIR
 cd angular
 docker-compose build
+#docker-compose run angular npm install --silent
--- bin/stop_django.sh
+++ bin/stop_django.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker stop $(docker ps -a -q  --filter ancestor=django-clonecademy:dev)
+docker stop $(docker ps -a -q  --filter ancestor=postgres)
