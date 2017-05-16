e0ff58457a86ee6eebd965206b910169c35e834b
Leonhard Wiedmann <lwiedmann@gmx.de>
2017-05-17 01:35:22 +0200

    npm install added after build

--- bin/build_angular.sh
+++ bin/build_angular.sh
@@ -5,4 +5,4 @@ DIR=$(find / -type d -name 'clonecadamy' 2>/dev/null -print -quit)
 cd $DIR
 cd angular
 docker-compose build
-#docker-compose run angular npm install --silent
+docker-compose run angular npm install --silent
