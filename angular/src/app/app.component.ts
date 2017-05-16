7d6d6bfc3da2c5f2f0a6945c3050f20fbbb5d450
Leonhard Wiedmann <lwiedmann@gmx.de>
2017-05-17 01:02:09 +0200

    TestComponent removed

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { TestComponent } from './test/test.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
