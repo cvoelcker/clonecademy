d8ff7e4b77341a82139820b3af0a5ad4bcf58184
Leonhard Wiedmann <lwiedmann@gmx.de>
2017-06-15 11:41:16 +0200

    QS text for usability added

--- bin/clonecademy_dev.sh
+++ bin/clonecademy_dev.sh
@@ -3,16 +3,16 @@
 start_django
 start_angular
 
-if hash atom 2>/dev/null; then
-  echo "Do you want to start atom in the clonecademy dir? Y/n"
-  read answer
-  if [ ! -z $answer ]
-    then
-    if [ $answer == "n" ] || [ $answer == "N" ]
-      then
-        exit
-    fi
-  fi
-  DIR=$(dirname $(dirname $(readlink -f ${BASH_SOURCE[0]})))
-  atom $DIR
-fi
+# if hash atom 2>/dev/null; then
+#   echo "Do you want to start atom in the clonecademy dir? Y/n"
+#   read answer
+#   if [ ! -z $answer ]
+#     then
+#     if [ $answer == "n" ] || [ $answer == "N" ]
+#       then
+#         exit
+#     fi
+#   fi
+#   DIR=$(dirname $(dirname $(readlink -f ${BASH_SOURCE[0]})))
+#   atom $DIR
+# fi
