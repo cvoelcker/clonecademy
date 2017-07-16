fa2807fe33e04bd49bb9666ced3e2f8dfa89320a
cvoelcker (iliricon) <claas@voelcker.net>
2017-07-16 13:09:13 +0200

    Updated tests for mod request
    Fixes the tests to work

--- django/learning_base/tests.py
+++ django/learning_base/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.utils import timezone
+
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import force_authenticate
 
@@ -11,7 +13,7 @@ class MultiCourseViewTest(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.view = views.MultiCourseView.as_view()
-        
+
         self.u1 = User(username='admin')
         self.u1.save()
 
@@ -29,8 +31,8 @@ class MultiCourseViewTest(TestCase):
     def test_post(self):
         c1_test_en_serialized = serializers.CourseSerializer(self.c1_test_en).data
 
-        request_1 = self.factory.post('/courses/', {'type':'all', 'language':'de', 'category':'all'})
-        request_2 = self.factory.post('/courses/', {'type':'all', 'language':'en', 'category':'all'})
+        request_1 = self.factory.post('/courses/', {'type':'', 'language':'de', 'category':''})
+        request_2 = self.factory.post('/courses/', {'type':'', 'language':'en', 'category':''})
         request_3 = self.factory.post('/courses/', {'stuff':'kajiger'})
 
         force_authenticate(request_1, self.u1)
@@ -40,21 +42,21 @@ class MultiCourseViewTest(TestCase):
         response_1 = self.view(request_1)
         response_2 = self.view(request_2)
         response_3 = self.view(request_3)
-        
+
         self.assertEqual(response_1.status_code,200)
         self.assertEqual(response_1.data, [])
 
         self.assertEqual(response_2.status_code,200)
         self.assertEqual(response_2.data, [c1_test_en_serialized])
 
-        self.assertEqual(response_3.status_code,404)
+        self.assertEqual(response_3.status_code,400)
 
 
 class CourseViewTest(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.view = views.CourseView.as_view()
-        
+
         self.u1 = User(username='admin')
         self.u1.save()
 
@@ -64,17 +66,18 @@ class CourseViewTest(TestCase):
         self.c1_test_en.save()
 
     def test_get(self):
+        c1_test_en_serialized = serializers.CourseSerializer(self.c1_test_en).data
         request = self.factory.get('/courses/1')
         force_authenticate(request, self.u1)
         response = self.view(request, course_id=1)
         self.assertEqual(response.status_code,200)
-        self.assertEqual(response.data, {'name': 'test_1', 'difficulty': 0, 'id': 1, 'modules': []})
+        self.assertEqual(response.data, c1_test_en_serialized)
 
     def test_post(self):
-        request = self.factory.post('/courses/save', 
-                   {'name':'test_2', 
+        request = self.factory.post('/courses/save',
+                   {'name':'test_2',
                     'category':'test',
-                    'difficulty': 2, 
+                    'difficulty': 2,
                     'modules':[],
                     'language':'en'}, format='json')
         force_authenticate(request, self.u1)
@@ -159,11 +162,83 @@ class CourseViewTest(TestCase):
                     'language':'en'}, format='json')
         force_authenticate(request, self.u1)
         response = self.view(request)
-        print(response.data)
         self.assertEqual(response.status_code,201)
         self.assertTrue(models.Course.objects.filter(name='test_4').exists())
         self.assertTrue(multiple_choice.models.MultipleChoiceQuestion.objects.filter(title='a question').exists())
 
+
+class RequestViewTest(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = views.RequestView.as_view()
+        
+        self.mod_group = Group(name="moderator")
+        self.mod_group.save()
+
+        self.u1 = User(username='user1')
+        self.u1.save()
+        self.u1_profile = models.Profile(user=self.u1)
+        self.u1_profile.save()
+        self.u2 = User.objects.create(username='mod')
+        self.u2.groups.add(self.mod_group)
+        self.u2.save()
+        self.u2_profile = models.Profile(user=self.u2)
+        self.u2_profile.save()
+        self.u3 = User.objects.create(username="spamer")
+        self.u3.save()
+        self.u3_profile = models.Profile(user=self.u3)
+        self.u3_profile.save()
+        self.u3.profile.last_modrequest = timezone.localdate()
+
+    def test_get(self):
+        "Test for true positive"
+        request_1 = self.factory.get('/user/can_request_mod')
+        force_authenticate(request_1, self.u1)
+        response = self.view(request_1)
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.data, True)
+
+        "Test for true negative"
+        request_2 = self.factory.get('/user/can_request_mod')
+        force_authenticate(request_2, self.u2)
+        response = self.view(request_2)
+        self.assertEqual(response.status_code,200)
+        self.assertTrue(not response.data)
+
+        "Test for true negative"
+        request_3 = self.factory.get('/user/can_request_mod')
+        force_authenticate(request_3, self.u3)
+        response = self.view(request_3)
+        self.assertEqual(response.status_code,200)
+        self.assertTrue(not response.data)
+
+
+    def test_post(self):
+        request_1 = self.factory.post("user/request_mod",
+                   {"reason":"you need me"}, format='json')
+        force_authenticate(request_1, self.u1)
+        response = self.view(request_1)
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.data, {"Request": "ok"})
+        self.assertTrue(self.mod_group in self.u1.groups.all())
+
+        request_2 = self.factory.post("user/request_mod",
+                   {"reason":"you need me"}, format='json')
+        force_authenticate(request_2, self.u2)
+        response = self.view(request_2)
+        self.assertEqual(response.status_code,403)
+        self.assertEqual(response.data, {"Request": "ok"})
+        self.assertTrue(self.mod_group in self.u1.groups.all())
+
+        request_3 = self.factory.post("user/request_mod",
+                   {"reason":"you need me"}, format='json')
+        force_authenticate(request_3, self.u3)
+        response = self.view(request_3)
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.data, {"Request": "ok"})
+        self.assertFalse(self.mod_group in self.u1.groups.all())
+
+        pass
 
 #factory = APIRequestFactory()
 #request = factory.post('/notes/', {'title': 'new idea'}, format='json')
