8a46b7c8a28e9eefd553f7fff4f0e45d53595cb2
Leonhard Wiedmann <lwiedmann@gmx.de>
2017-08-17 17:27:55 +0200

    small issues fixed

--- angular/src/app/image-cropper/image-cropper.component.ts
+++ angular/src/app/image-cropper/image-cropper.component.ts
@@ -23,6 +23,7 @@ export class ImageCropperDialogComponent{
 
   constructor(@Optional() @Inject(MD_DIALOG_DATA) public input: {height: number, width: number}, public dialogRef: MdDialogRef<ImageCropperDialogComponent>) {
       this.cropperSettings = new CropperSettings();
+      this.cropperSettings.noFileInput = true;
       this.cropperSettings.fileType = "image/*"
       if(input.width && input.height ){
         this.cropperSettings.canvasWidth = 500;
@@ -30,20 +31,18 @@ export class ImageCropperDialogComponent{
         this.cropperSettings.croppedWidth = input.width;
         this.cropperSettings.croppedHeight = input.height;
       }
-      this.cropperSettings.noFileInput = true;
       this.data = {};
   }
 
   fileChangeListener($event) {
+    this.cropper.reset()
     var image:any = new Image();
     var file:File = $event.target.files[0];
     var myReader:FileReader = new FileReader();
     var that = this;
     myReader.onloadend = function (loadEvent:any) {
-        image.src = loadEvent.target.result;
-        //that.data = image;
-        that.cropper.setImage(image);
-
+      image.src = loadEvent.target.result;
+      that.cropper.setImage(image);
     };
       myReader.readAsDataURL(file);
   }
