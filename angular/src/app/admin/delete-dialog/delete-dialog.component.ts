7ea907c9a73b722cfc973152e90f3101ea6cb363
Tobias Huber <tobias.huber@stud.tu-darmstadt.de>
2017-09-08 09:03:35 +0200

    added a (not workin) dialog

--- angular/src/app/admin/delete-dialog/delete-dialog.component.ts
+++ angular/src/app/admin/delete-dialog/delete-dialog.component.ts
@@ -1,106 +1,22 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MdDialog } from '@angular/material';
+import { Component, Inject, Optional } from '@angular/core';
 
-import { ServerService } from '../../service/server.service';
-
-import { Router } from "@angular/router"
-/*
-import { UserDetailComponent } from '../user-detail/user-detail.component'
-*/
-//import { UserDetailComponent } from '../user-detail/user-detail.component'
+import {MD_DIALOG_DATA} from '@angular/material';
 
 @Component({
-  selector: 'app-course-categories',
-  templateUrl: './course-categories.component.html',
-  styleUrls: ['./course-categories.component.sass']
+  selector: 'app-delete-dialog',
+  templateUrl: './delete-dialog.component.html',
+  styleUrls: ['./delete-dialog.component.sass']
 })
-export class CourseCategoriesComponent implements OnInit {
+export class DeleteDialogComponent {
 
-  loading = true;
-  create = false;
+  courses: any;
 
-  selected: any;
-
-  categories: any;
-
-  error = false;
-  errorMessage = "";
-
-
-
-  constructor(private server: ServerService, private router: Router,
-              public dialog: MdDialog) { }
-
-  ngOnInit() {
-    // load the data for all categories
-
-    this.server.get("get-course-categories/", true)
-      .then(data => {
-        this.categories = data;
-        this.loading = false;
-      })
-  }
-
-  change(c: any){
-    this.selected = c;
-    this.create = false;
-    console.log("change called")
-  }
-
-  openCreate(){
-    console.log("open create called")
-    this.create = true;
-  }
-
-  delete(){
-    this.openDialog()
-    /*
-    this.server.post('get-course-categories/',{
-      "delete": "true",
-      "id": this.selected.id}
-    )*/
-  }
-  // register the updated category
-  register(value){
-    console.log(value)
-    if (value.valid) {
-      let data = value.value
-      if (data['categorycolor']=='')
-        delete data['categorycolor'];
-      if (data['categoryname']=='')
-        delete data['categoryname'];
-      if (this.selected != undefined && !this.create) {
-        data["id"] = this.selected.id;
-      }
-      this.server.post('get-course-categories/', data, false, false)
-        .then(answer => {
-          this.selected = answer;
-          this.categories.push(answer);
-        })
-        .catch(errorRes => {
-          this.error = true;
-          this.errorMessage=errorRes;
-        })
-      if (this.create) {
-        this.change(this.selected)
-      }
+  constructor(@Optional() @Inject(MD_DIALOG_DATA) public data: string) {
+    if(data != undefined){
+      this.courses=data
     }
   }
 
-  openDialog() {
-    const dialogRef = this.dialog.open(DialogContentDeleteDialogComponent, {
-      height: '350px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
+  ngOnInit() {
   }
 };
-
-@Component({
-  selector: 'app-course-categories',
-  templateUrl: './delete-dialog.html',
-  styleUrls: ['./course-categories.component.sass']
-})
-export class DialogContentDeleteDialogComponent {}
