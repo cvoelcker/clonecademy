8ed4d579531d352d482df08c8a0f7a1eadb9d6c6
cvoelcker (iliricon) <claas@voelcker.net>
2017-09-12 12:37:13 +0200

    Added automated test and some output

---
+++ orga/QS-Document/test.sh
@@ -0,0 +1,65 @@
+git checkout 22f76fcce2d1d6b03857a9a4db7f9276433aecdd
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_22f76.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_22f76.txt
+cd ..
+
+git checkout cf525f64091e6c892b7e471fc8048bd8c056d69e
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_cf525.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_cf525.txt
+cd ..
+
+git checkout e2b7fed9dbc0e929bb28e7ccf30cd68aa0d73c19
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_e2b7.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_e2b7.txt
+cd ..
+
+git checkout f5845985bf7463a32f0f4f04279ea7481a32daaf
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_f584.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_f584.txt
+cd ..
+
+git checkout 2d17b38eecc8bbd754534a564ff2a22d60ee5f71
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_2d17b.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_2d17b.txt
+cd ..
+
+git checkout 7af0e8284c862f9841d8101c6f4569adb0c6289c
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_7af0e.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_7af0e.txt
+cd ..
+
+git checkout b5e9a319bf3bd0bc33e544ff615e1bd8b62c7931
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_b5e9a.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_b5e9a.txt
+cd ..
+
+git checkout ba0fab4c21d736b0f67f8fe21ef90c0da810ac4b
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_ba0fa.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_ba0fa.txt
+cd ..
+
+git checkout 78f8954ebc159fbcb6ed6a767e18d3437a604d22
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_78f89.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_78f89.txt
+cd ..
+
+git checkout 11bc7fa37a20ac045d5a6c29e4e3a060b73b3504
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_11bc7.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_11bc7.txt
+cd ..
+
+git checkout ffc8afbf7012f4b2d63e5b58ac8f0c224a7ee382
+bandit -r django > ../clonecadamy/orga/QS-Document/bandit/bandit_ffc8a.txt
+cd angular
+npm run ng lint > ../../clonecadamy/orga/QS-Document/ng_lint/ng_lint_ffc8a.txt
+cd ..
